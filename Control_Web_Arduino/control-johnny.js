6ee21038bade54d4bd166eaf740a28ed678b756d
fredpatch <f.patchelli@gmail.com>
2023-04-09 14:55:36 +0100

    Control Web
    Web control of the arduino

---
+++ Control_Web_Arduino/control-johnny.js
@@ -0,0 +1,73 @@
+const express = require('express');
+const five = require('johnny-five');
+
+
+const app = express();
+const port = 3000;
+
+// Serve the HTML file for the website
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.sendFile('index.html');
+})
+
+// Set up the routes for the automatic and manual buttons
+app.get('/automatic', (req, res) => {
+  // Code to turn on the LED automatically based on light level
+  if (luxmeter.value > 250) {
+    led.off();
+  } else {
+    led.on();
+  }
+  res.send('Automatic mode activated');
+});
+
+app.get('/manual', (req, res) => {
+  // Code to turn on the LED manually
+  led.on();
+  res.send('Manual mode activated');
+});
+
+
+
+const board = new five.Board({
+  port: 'COM6'
+});
+
+let led;
+let luxmeter;
+
+board.on('ready', () => {
+  // Define the LED pin
+  led = new five.Led(13);
+
+  // Define the Luxmeter pin
+  luxmeter = new five.Sensor({
+    pin: 'A0',
+    freq: 250
+  });
+  
+  luxmeter.on("data", function(){
+
+    ldrvalue = (luxmeter.value /1024) *100;
+    console.log(luxmeter.value);
+    
+    });
+  
+    app.get('/ldr', (req, res) => {
+        res.send(ldrvalue.toString());
+        });
+
+        app.get('/sensor', (req, res, next) => {
+            res.json({
+                sensor: parseFloat( ((luxmeter?.value / 1024) * 100)+'').toFixed(2)
+            });
+        });
+
+  // Start the server
+  app.listen(port, () => {
+    console.log(`Server listening at http://localhost:${port}`);
+  });
+
+});
